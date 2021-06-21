98179768b3ae625c8ec72d68612434b3079516ca
Clyde <squall1247@gmail.com>
2021-06-22 04:01:34 +0800

    Use Stub to test

--- root/backend/test/middleware_auth.js
+++ root/backend/test/middleware_auth.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -19,5 +21,31 @@ describe('Auth middleware test', function() {
             }
         };
         expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw();
+    });
+
+    it ('Should yield an userId after decoding the token', function(){
+        const req = {
+            get: function(headerName) {
+                return 'Bearer abcde';
+            }
+        };
+        //using stub
+        sinon.stub(jwt, 'verify');
+        jwt.verify.returns( {userId: 'abc'} );
+
+        authMiddleware(req, {} , ()=>{});
+        expect(req).to.have.property('userId');
+        expect(jwt.verify.called).to.be.true;
+
+        jwt.verify.restore();
+    });
+
+    it ('Should throw an error if the token canot be verified', function(){
+        const req = {
+            get: function(headerName) {
+                return 'Bearer abcde';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
     });
 });
